a0af8f165eb1f221ea00ddc4eda7afa718a187bd
WojciechBuda <wojciech.buda6@gmail.com>
2023-07-04 18:24:38 +0200

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,31 @@
+const key = 'oopdupy7FALGO9RiAgNfjLv8OA0jOMKJ'
+
+
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0]; 
+
+};
+
+// getCity('warsaw').then(data => {
+//         return getWeather(data.Key)
+// }).then(data => {
+//     console.log(data);
+// })
+// .catch(err => console.log(err));
+
